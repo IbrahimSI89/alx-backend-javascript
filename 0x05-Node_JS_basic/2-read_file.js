847adbab8d1845ed21389de2d0edef99b42a1a2f
Hima <ibrahimbusiness89211@gmail.com>
2024-07-27 03:59:21 +0300

    4 5

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.trim().split('\n').filter(line => line);
+    const lines = data.trim().split('\n').filter((line) => line);
 
     const totalStudents = lines.length - 1;
     console.log(`Number of students: ${totalStudents}`);
 
     const fields = {};
-    for (let i = 1; i < lines.length; i++) {
+    for (let i = 1; i < lines.length; i += 1) { // Fixed the no-plusplus error
       const [firstName, , , field] = lines[i].split(',');
       if (!fields[field]) {
         fields[field] = [];
@@ -18,7 +18,9 @@ function countStudents(path) {
     }
 
     for (const field in fields) {
-      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      if (Object.prototype.hasOwnProperty.call(fields, field)) { // Fixed the guard-for-in error
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
     }
   } catch (err) {
     throw new Error('Cannot load the database');
