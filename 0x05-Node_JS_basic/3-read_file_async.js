5a10b1a23e5e94fb0391e30053264c7f5fecf743
Hima <ibrahimbusiness89211@gmail.com>
2024-07-27 04:02:21 +0300

    4 5

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -6,13 +6,13 @@ function countStudents(path) {
       if (err) {
         reject(new Error('Cannot load the database'));
       } else {
-        const lines = data.trim().split('\n').filter(line => line);
+        const lines = data.trim().split('\n').filter((line) => line);
 
         const totalStudents = lines.length - 1;
         console.log(`Number of students: ${totalStudents}`);
 
         const fields = {};
-        for (let i = 1; i < lines.length; i++) {
+        for (let i = 1; i < lines.length; i += 1) { // Fixed the no-plusplus error
           const [firstName, , , field] = lines[i].split(',');
           if (!fields[field]) {
             fields[field] = [];
@@ -21,7 +21,9 @@ function countStudents(path) {
         }
 
         for (const field in fields) {
-          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+          if (Object.prototype.hasOwnProperty.call(fields, field)) { // Fixed the guard-for-in error
+            console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+          }
         }
 
         resolve();
